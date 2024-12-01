1b5b31b4c9daf77d4edef180215ab2f0fdbc5eae
Keerthana1617 <ks7208889@gmail.com>
2024-12-01 23:29:05 +0530

    Day7

---
+++ Student.java
@@ -0,0 +1,52 @@
+package Day7;
+
+public class Student {
+	String name=" ";
+	int age;
+	String dateofBirth;
+	int rollNo;
+	
+	
+	public static void main(String args[]) {
+		Student student1= new Student();
+		student1.name="Keerthana";
+		student1.age=21;
+		student1.dateofBirth="16/07/2003";
+		student1.rollNo=216080111;
+		System.out.println(student1.name);
+		System.out.println(student1.age);
+		System.out.println(student1.dateofBirth);
+		System.out.println(student1.rollNo);
+				
+		
+
+		Student student2= new Student();
+		student2.name="Pavithra";
+		student2.age=21;
+		student2.dateofBirth="09/11/2003";
+		student2.rollNo=216080112;
+		System.out.println(student2.name);
+		System.out.println(student2.age);
+		System.out.println(student2.dateofBirth);
+		System.out.println(student2.rollNo);
+				
+				
+				
+		Student student3= new Student();
+		student3.name="Sri brama";
+		student3.age=21;
+		student3.dateofBirth="25/05/2003";
+		student3.rollNo=216080113;
+		System.out.println(student3.name);
+		System.out.println(student3.age);
+		System.out.println(student3.dateofBirth);
+		System.out.println(student3.rollNo);
+				
+		
+	}
+	
+
+}
+
+
+
