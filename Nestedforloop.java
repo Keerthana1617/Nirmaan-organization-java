58a9975baa913721006df5c5853866c2ee68fc25
Keerthana1617 <ks7208889@gmail.com>
2024-12-01 23:27:49 +0530

    Add files via upload
    Day6

---
+++ Nestedforloop.java
@@ -0,0 +1,14 @@
+package Day6;
+
+public class Nestedforloop {
+	public static void main(String args[])
+	{
+		for(int i=1;i<=20;i++) {
+			for(int j=1;j<=4;j++) {
+				System.out.println(j);
+				
+				}
+			}
+		}
+	}
+
