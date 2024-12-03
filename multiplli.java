bc1fc57d5505f527a8bc43281b5cd2da84106a61
Keerthana1617 <ks7208889@gmail.com>
2024-12-03 13:00:49 +0530

    Day10

---
+++ multiplli.java
@@ -0,0 +1,18 @@
+package Day10;
+
+public class multiplli {
+	public static void main(String args[]) {
+		for(int i=0;i<=3;i++) {
+			
+			
+			for(int j=0;j<=3;j++) {
+				System.out.print("*\t");
+				
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+
