48d7291b79d5640b188b37e5bcd43eac1b04a0ea
Keerthana1617 <ks7208889@gmail.com>
2024-12-06 11:27:15 +0530

    Day 11

---
+++ Encapsulation/NiramaanOrganiZation.java
@@ -0,0 +1,20 @@
+package Day11;
+
+public class NiramaanOrganiZation {
+	public static void main(String args[]) {
+		Company com=new Company();
+		com.setEmpId(1234);
+		com.setName("Sri");
+		com.setAge(18);
+		com.setDept("csc");
+		System.out.println(com.getEmpId());
+		System.out.println(com.getName());
+		System.out.println(com.getAge());
+		System.out.println(com.getDept());
+		System.out.println(com.toString());
+		
+				
+	}
+
+}
+
